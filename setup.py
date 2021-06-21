d7b6baf1c9454262b2ce46902efa090e758aa4ed
Mateusz Konieczny <matkoniecz@gmail.com>
2021-06-21 14:13:30 +0200

    rename package to target name

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="example_pkg_matkoniecz",
+    name="leafleter",
     version="0.1.3",
     author="Example Author",
     author_email="author@example.com",
